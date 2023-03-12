f1aa0a801cd1554876848a3215bee52a603692b7
Juan Gotti <94028705+OneGoatee@users.noreply.github.com>
2023-03-12 10:10:24 -0500

    Add Switch Statement to updateDisplay Function

--- script.js
+++ script.js
@@ -16,12 +16,51 @@ function updateDisplay(button) {
     displayText.innerText = '';
   }
 
-  if (button.classList.contains('ac')) {
-    allClear();
-  }
+  switch (true) {
+    case button.classList.contains('number'):
+      displayText.innerText += button.innerText;
+      console.log(`Its a number`);
+      break;
 
-  if (button.classList.contains('number')) {
-    displayText.innerText += button.innerText;
+    case button.classList.contains('ac'):
+      allClear();
+      console.log(`Its All Clear`);
+      break;
+
+    case button.classList.contains('plus-minus'):
+      console.log(`Its Plus Minus`);
+      break;
+
+    case button.classList.contains('percent'):
+      console.log(`Its Percent`);
+      break;
+
+    case button.classList.contains('division'):
+      console.log(`Its Division`);
+      break;
+
+    case button.classList.contains('multiplication'):
+      console.log(`Its Multiplication`);
+      break;
+
+    case button.classList.contains('subtraction'):
+      console.log(`Its Subtraction`);
+      break;
+
+    case button.classList.contains('addition'):
+      console.log(`Its Addition`);
+      break;
+
+    case button.classList.contains('equal'):
+      console.log(`Its Equal`);
+      break;
+
+    case button.classList.contains('decimal'):
+      console.log(`Its Decimal`);
+      break;
+
+    default:
+      break;
   }
 }
 
